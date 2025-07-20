d608134ed5b1359c06df9450d5c74e213dfd2652
shahzaib khurshid <48204885+shahzaibsheikh@users.noreply.github.com>
2025-07-20 19:58:18 +0500

    DB

---
+++ laravel.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2025 at 04:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+-- Error reading structure for table laravel.bookings: #1932 - Table &#039;laravel.bookings&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`bookings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_types`
+--
+-- Error reading structure for table laravel.class_types: #1932 - Table &#039;laravel.class_types&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.class_types: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`class_types`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table laravel.failed_jobs: #1932 - Table &#039;laravel.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table laravel.jobs: #1932 - Table &#039;laravel.jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table laravel.migrations: #1932 - Table &#039;laravel.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table laravel.password_reset_tokens: #1932 - Table &#039;laravel.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table laravel.personal_access_tokens: #1932 - Table &#039;laravel.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scheduled_classes`
+--
+-- Error reading structure for table laravel.scheduled_classes: #1932 - Table &#039;laravel.scheduled_classes&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.scheduled_classes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`scheduled_classes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_entries`
+--
+-- Error reading structure for table laravel.telescope_entries: #1932 - Table &#039;laravel.telescope_entries&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.telescope_entries: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`telescope_entries`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_entries_tags`
+--
+-- Error reading structure for table laravel.telescope_entries_tags: #1932 - Table &#039;laravel.telescope_entries_tags&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.telescope_entries_tags: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`telescope_entries_tags`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_monitoring`
+--
+-- Error reading structure for table laravel.telescope_monitoring: #1932 - Table &#039;laravel.telescope_monitoring&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.telescope_monitoring: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`telescope_monitoring`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table laravel.users: #1932 - Table &#039;laravel.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table laravel.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravel`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
